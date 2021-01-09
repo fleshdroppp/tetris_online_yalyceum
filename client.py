574d4a0b4a33e697e56ed0b0a5706028e70f723a
fleshdroppp <khusnutdinovakim@gmail.com>
2021-01-09 14:05:52 +0000

    Fixed client

--- client.py
+++ client.py
@@ -1,146 +1,62 @@
 import socket
+import pygame
 import json
 import pickle
-from random import randrange, choice
-from copy import deepcopy
-import pygame
+running = True
 
-users = []
-clock = pygame.time.Clock()
+WIDTH, HEIGHT = 10, 20
+CELL_SIZE = 45
+
+GAME_RESOLUTION = WIDTH * CELL_SIZE, HEIGHT * CELL_SIZE
+WINDOW_RESOLUTION = 750, 940
 FPS = 60
+win_screen = pygame.display.set_mode(WINDOW_RESOLUTION)
+screen = pygame.Surface(GAME_RESOLUTION)
+clock = pygame.time.Clock()
 
+pygame.init()
+grid = [pygame.Rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE) for x in range(WIDTH) for y in range(HEIGHT)]
 
-def send_blocks(data):
-    clock.tick(FPS)
-    data_string = pickle.dumps(data)
-    users[0].send(data_string)
-
-
-def game():
-    WIDTH, HEIGHT = 10, 20
-    CELL_SIZE = 45
-    WINDOW_RESOLUTION = 750, 940
-
-    GAME_RESOLUTION = WIDTH * CELL_SIZE, HEIGHT * CELL_SIZE
-
-    # win_screen = pygame.display.set_mode(WINDOW_RESOLUTION)
-    # screen = pygame.Surface(GAME_RESOLUTION)
-
-
-
-    pygame.init()
-
-    figures_types = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
-                     [(0, -1), (-1, -1), (-1, 0), (0, 0)],
-                     [(-1, 0), (-1, 1), (0, 0), (0, -1)],
-                     [(0, 0), (-1, 0), (0, 1), (-1, -1)],
-                     [(0, 0), (0, -1), (0, 1), (-1, -1)],
-                     [(0, 0), (0, -1), (0, 1), (1, -1)],
-                     [(0, 0), (0, -1), (0, 1), (-1, 0)]]
-
-    anim_count, anim_speed, anim_limit = 0, 60, 2000
-
-    figures = [[pygame.Rect(x + WIDTH // 2, y + 1, 1, 1) for x, y in pos] for pos in figures_types]
-    figure_rect = pygame.Rect(0, 0, CELL_SIZE - 2, CELL_SIZE - 2)
-    board = [[0 for col in range(WIDTH)] for row in range(HEIGHT)]
-
-    colors = ['#00ff00', '#ff0000', '#ffffff', '#ADD8E6', '#f7ca18', '#bf55ec', '#F08080']
-    current_figure, next_figure = deepcopy(choice(figures)), deepcopy(choice(figures))
-    color, next_color = colors[figures.index(current_figure)], colors[figures.index(next_figure)]
-    score = 0
-
-    def check_pos():
-        if current_figure[cell].x < 0 or current_figure[cell].x >= WIDTH:
-            return False
-        elif current_figure[cell].y >= HEIGHT or \
-                board[current_figure[cell].y][current_figure[cell].x]:
-            return False
-        return True
-
-    running = True
+if __name__ == '__main__':
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 4006))
 
     while running:
-        blocks = []
+        win_screen.fill('black')
+        win_screen.blit(screen, (20, 20))
+        screen.fill('black')
+
         d_x, rotation = 0, False
-        event = users[0].recv(2048).decode()
-        print(event)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    client.send('right'.encode('utf-8'))
+                elif event.key == pygame.K_LEFT:
+                    client.send('left'.encode('utf-8'))
+                elif event.key == pygame.K_DOWN:
+                    client.send('down'.encode('utf-8'))
+                elif event.key == pygame.K_UP:
+                    client.send('rotate'.encode('utf-8'))
+            if event.type == pygame.QUIT:
+                client.send('quit'.encode('utf-8'))
 
-        if event == 'right':
-            d_x += 1
-        if event == 'left':
-            d_x -= 1
-        if event == 'quit':
-            running = False
-        if event == 'rotate':
-            rotation = True
-        if event == 'down':
-            anim_limit = 100
+        [pygame.draw.rect(screen, (50, 50, 50), item, 1) for item in grid]
 
-        # ПЕРЕДВИЖЕНИЕ ПО ОСИ Х
-        old_figure = deepcopy(current_figure)
-        for cell in range(4):
-            current_figure[cell].x += d_x
-            if not check_pos():
-                current_figure = deepcopy(old_figure)
-                break
+        client.send('ready'.encode())
 
-        # ПЕРЕДВИЖЕНИЕ ПО ОСИ У
-        anim_count += anim_speed
-        if anim_count > anim_limit:
-            anim_count = 0
-            old_figure = deepcopy(current_figure)
-            for cell in range(4):
-                current_figure[cell].y += 1
-                if not check_pos():
-                    for i in range(4):
-                        board[old_figure[i].y][old_figure[i].x] = color
-                    current_figure, color = next_figure, next_color
-                    next_figure = deepcopy(choice(figures))
-                    next_color = colors[figures.index(next_figure)]
-                    anim_limit = 2000
-                    break
+        data = client.recv(4096)
+        blocks = pickle.loads(data)
 
-        # Поворот фигуры
-        center = current_figure[0]
-        old_figure = deepcopy(current_figure)
-        if rotation:
-            for cell in range(4):
-                x = current_figure[cell].y - center.y
-                y = current_figure[cell].x - center.x
-                current_figure[cell].x = center.x - x
-                current_figure[cell].y = center.y + y
-                if not check_pos():
-                    current_figure = deepcopy(old_figure)
-                    break
+        for block in blocks:
+            if block[2] == 1:
+                pygame.draw.rect(screen, block[1], block[0])
 
-        for cell in range(4):
-            figure_rect.x = current_figure[cell].x * CELL_SIZE + 1
-            figure_rect.y = current_figure[cell].y * CELL_SIZE + 1
-            blocks.append([deepcopy(figure_rect), color, 1])
+        pygame.display.flip()
+        clock.tick(FPS)
 
-        for y, row in enumerate(board):
-            for x, col in enumerate(row):
-                if col:
-                    figure_rect.x, figure_rect.y = x * CELL_SIZE, y * CELL_SIZE
-                    blocks.append([deepcopy(figure_rect), col, 1])
-
-        if event == 'ready':
-            send_blocks(blocks)
-
-
-
-        # pygame.display.flip()
-
-
-if __name__ == "__main__":
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('127.0.0.1', 4006))
-    server.listen(2)
-
-    while len(users) != 1:
-        user_sock, address = server.accept()
-        users.append(user_sock)
-
-    game()
-    server.close()
-
+    #     [pygame.draw.rect(screen, (50, 50, 50), item, 1) for item in grid]
+    #     pygame.display.flip()
+    #     clock.tick(FPS)
+    # for block in blocks:
+    #     print(block)
+    #     pygame.draw.rect(block[2], block[1], block[0])
